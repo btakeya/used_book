05150fa7bd9a4714f4b76477b0edbd8313fe0fa7
Juhwan Yun <btakeya.dev@gmail.com>
2016-08-07 16:55:35 +0900

    Add QueryBuilder

---
+++ app/QueryBuilder.py
@@ -0,0 +1,44 @@
+import requests
+from urllib.parse import urlencode
+from urllib.request import urlretrieve
+
+class RequestBuilder(object):
+    _domain = 'http://off.aladin.co.kr'
+    _path = 'usedstore/wsearchresult.aspx'
+
+    PARAM_NAME_KEYWORD = 'SearchWord'
+    PARAM_NAME_X = 'x'
+    PARAM_NAME_Y = 'Y'
+
+
+    def __init__(self):
+        self.url_params = {}
+        self.cookie_params = {}
+
+    def set_params(self, key, value):
+        self.url_params[key] = value
+        return self
+
+    def set_store(self, storeCode):
+        self.cookie_params['OffCode'] = storeCode
+        return self
+
+    def make_params(self, key, x, y):
+        return dict(SearchWord=key, x=x, y=y)
+
+    def get(self):
+        cookies = dict(AladinUsedStore=urlencode(self.cookie_params))
+
+        return requests.get('{}/{}'.format(self._domain, self._path),
+                            cookies=cookies, params=self.url_params)
+
+
+if __name__ == '__main__':
+    req_builder = RequestBuilder()
+    req_builder.set_params(req_builder.PARAM_NAME_KEYWORD, 'hello') \
+               .set_params(req_builder.PARAM_NAME_X, 1) \
+               .set_params(req_builder.PARAM_NAME_Y, 2) \
+               .set_store('bundang')
+    res = req_builder.get()
+
+
