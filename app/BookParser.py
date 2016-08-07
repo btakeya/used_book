22a14807d27bbf54ffbd92f724ec49ba87ee0e84
Juhwan Yun <btakeya.dev@gmail.com>
2016-08-07 17:12:19 +0900

    Add BookParser

---
+++ app/BookParser.py
@@ -0,0 +1,21 @@
+from bs4 import *
+
+class BookParser(object):
+    def __init__(self, result):
+        self.page = result.text
+        self.soup = BeautifulSoup(self.page, 'html.parser')
+
+    def parse(self):
+        results = []
+        divs = self.soup.findAll('div')
+        for div in divs:
+            div_class_list = div.get('class')
+            if type(div_class_list) is list and \
+               'ss_book_box' in div.get('class'):
+                search_text = div.get_text()
+                for line in search_text.split('\n'):
+                    if len(line) > 0:
+                        results.append(line)
+
+        for result in results:
+            print(result)
