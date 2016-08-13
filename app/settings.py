0e0c4a285dd16e943bb0f4b50e65eb8f1e5dc48e
Juhwan Yun <btakeya.dev@gmail.com>
2016-08-13 15:33:39 +0900

    Change directory name for static files

--- app/settings.py
+++ app/settings.py
@@ -117,5 +117,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL  = '/used-book/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'used-book')
+STATIC_URL  = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
